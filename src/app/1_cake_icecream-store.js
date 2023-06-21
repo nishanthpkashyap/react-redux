32186d07bef0c4f31bafba1481d3567159b77874
nishanth <nishanthpkashyap1503@gmail.com>
2023-06-21 16:52:03 +0530

    added app and features from redux-toolkit

---
+++ src/app/1_cake_icecream-store.js
@@ -0,0 +1,14 @@
+const { configureStore } = require("@reduxjs/toolkit");
+const { cakeReducer } = require("./features/cake/cakeSlice");
+const { icecreamReducer } = require("./features/ice-cream/1_icecreamSlice");
+
+const store = configureStore({
+  reducer: {
+    cake: cakeReducer,
+    icecream: icecreamReducer,
+  },
+});
+
+module.exports = {
+  store,
+};
